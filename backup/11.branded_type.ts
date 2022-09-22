b8e9fe34d5a0f887adad71d2c32d8ba3b3b327ae
Hoang <nguyenminhhoang210891@gmail.com>
2022-09-23 06:56:13 +0700

    slides export

--- backup/11.branded_type.ts
+++ backup/11.branded_type.ts
@@ -14,3 +14,6 @@ https://github.com/LeDDGroup/typescript-conditional-types
 https://github.com/LeDDGroup/ts-types-utils
 https://github.com/sindresorhus/type-fest
 https://www.typescriptlang.org/docs/handbook/utility-types.html
+https://dev.to/joanllenas/decoding-json-with-typescript-1jjc
+https://github.com/joanllenas/ts.data.json
+https://github.com/TCMiranda/joi-extract-type/blob/master/index.ts
